38bbd990157ee64229902f702dbdf37cc0254796
Vinod <vinodreddy1876@gmail.com>
2020-10-20 21:43:32 -0400

    Evaluating expression in react

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Person = () => {
-  return <p>I am a Person!</p>;
+  return (
+    <p>I am a Person and I am {Math.floor(Math.random() * 30)} years Old!</p>
+  );
 };
 
 export default Person;
