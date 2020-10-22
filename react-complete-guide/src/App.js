e274f3852c67b766bb0f533698a5fd972ef898e5
Vinod <vinodreddy1876@gmail.com>
2020-10-22 18:50:05 -0400

    Change App.js to functional component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,41 +1,43 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
-class App extends Component {
-  state = {
+const app = props => {
+  const [personState, updatePersonState] = useState({
     person: [
       { name: "Max", age: "26" },
       { name: "Manu", age: "28" }
     ],
-    otherState: "test"
-  };
+    otherState: "Test"
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const [otherUseState, updateOtherUseState] = useState("second use state");
+
+  console.log(personState);
+  const switchNameHandler = () => {
+    updatePersonState({
       person: [
         { name: "Maximillan", age: "26" },
         { name: "Manu", age: "29" }
-      ]
+      ],
+      otherState: "Test" //If not defined here, this state will be lost. To avoid this multiple useStates can be used.
     });
   };
+  console.log(personState.otherState);
+  console.log(otherUseState);
+  return (
+    <div className="App">
+      <h1>Hi, I am React App</h1>
+      {/* Having this.switchNameHandler() will invoke method call during page rendering itself. */}
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personState.person[0].name}
+        age={personState.person[0].age}
+      />
+      <Person name={personState.person[1].name} age={personState.person[1].age}>
+        My Hobbies: Racing
+      </Person>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I am React App</h1>
-        {/* Having this.switchNameHandler() will invoke method call during page rendering itself. */}
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person
-          name={this.state.person[0].name}
-          age={this.state.person[0].age}
-        />
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}>
-          My Hobbies: Racing
-        </Person>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default app;
