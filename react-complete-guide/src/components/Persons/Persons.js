ae61d2298c5e699048b49985e1ff710b4b0821c7
Vinod <vinodreddy1876@gmail.com>
2020-11-01 18:56:03 -0500

    shouldComponentUpdate example

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,18 +1,28 @@
 import Person from "./Person/Person";
-import React from "react";
-const persons = props =>
-  props.persons.map((person, index) => {
-    /* React uses key to compare list elements in virtual dom with past verson and render only changed elements. 
+import React, { Component } from "react";
+class Persons extends Component {
+  //Component will rerender if shouldComponentUpdate method returns true.
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.persons !== this.props.persons) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  /* React uses key to compare list elements in virtual dom with past verson and render only changed elements. 
       This is useful while rendering larger list elements as it avoids rendering whole list  */
-    return (
-      <Person
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        click={() => props.clicked(index)}
-        changed={event => props.changed(event, person.id)}
-      />
-    );
-  });
-
-export default persons;
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          click={() => this.props.clicked(index)}
+          changed={event => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
+}
+export default Persons;
