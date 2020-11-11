efa5805152d99101256d8f406ed9b80823fb7f97
Vinod <vinodreddy1876@gmail.com>
2020-11-11 10:41:54 -0500

    Add functionality to burger component

--- react-burger-builder/src/App.js
+++ react-burger-builder/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "../src/containers/BurgerBuilder/BurgerBuilder";
 function App() {
   return (
     <div>
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
